18c4d29ae1d3d3a3f047f655a0294f0961f60e9c
SweetAI Belle <SweetAIBelle@gmail.com>
2023-12-07 17:56:58 -0800

    Combine day header files.

--- src/aoc.cpp
+++ src/aoc.cpp
@@ -1,11 +1,5 @@
 #include "util/util.h"
-#include "day_1.h"
-#include "day_2.h"
-#include "day_3.h"
-#include "day_4.h"
-#include "day_5.h"
-#include "day_6.h"
-#include "day_7.h"
+#include "days.h"
 
 int main()
 {
---
+++ src/days.h
@@ -0,0 +1,73 @@
+#include "util/util.h"
+
+#pragma once
+
+namespace day_1_part_1
+{
+    void run();
+}
+
+namespace day_1_part_2
+{
+    void run();
+}
+
+namespace day_2_part_1
+{
+    void run();
+}
+
+namespace day_2_part_2
+{
+    void run();
+}
+
+namespace day_3_part_1
+{
+    void run();
+}
+
+namespace day_3_part_2
+{
+    void run();
+}
+
+namespace day_4_part_1
+{
+    void run();
+}
+
+namespace day_4_part_2
+{
+    void run();
+}
+
+namespace day_5_part_1
+{
+    void run();
+}
+
+namespace day_5_part_2
+{
+    void run();
+}
+
+namespace day_6_part_1
+{
+    void run();
+}
+
+namespace day_6_part_2
+{
+    void run();
+}
+
+namespace day_7_part_1
+{
+    void run();
+}
+
+namespace day_7_part_2
+{
+    void run();
+}
