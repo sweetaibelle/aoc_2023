5e32188bd35a2e946d6bc373b573d8071dd50d4c
SweetAI Belle <SweetAIBelle@gmail.com>
2023-12-07 17:32:55 -0800

    Indicate what's been finished.

--- aoc.cpp
+++ aoc.cpp
@@ -8,18 +8,18 @@
 
 int main()
 {
-    //day_1_part_1::run();
-    //day_1_part_2::run();
-    //day_2_part_1::run();
-    //day_2_part_2::run();
-    //day_3_part_1::run();
-    //day_3_part_2::run();
-    //day_4_part_1::run();
+    //day_1_part_1::run(); // done
+    //day_1_part_2::run(); // done
+    //day_2_part_1::run(); // done
+    //day_2_part_2::run(); // done
+    //day_3_part_1::run(); // done
+    //day_3_part_2::run(); // done
+    //day_4_part_1::run(); // done
     //day_4_part_2::run();
     //day_5_part_1::run();
     //day_5_part_2::run();
-    //day_6_part_1::run();
-    day_6_part_2::run();
+    //day_6_part_1::run(); // done
+    //day_6_part_2::run(); // done
 
     return 0;
 }
